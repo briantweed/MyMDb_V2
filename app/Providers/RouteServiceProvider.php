8616e596435dc8959a10ae13b05b0c2b34a82b54
brtweed@outlook.com <brtweed@outlook.com>
2019-02-04 14:01:14 +0000

    no message

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,72 +2,95 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\Router;
+use Symfony\Component\Finder\Finder;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
+
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
+
     protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
+    protected $folder = 'routes/web';
+
+
     public function boot()
     {
-        //
-
         parent::boot();
     }
 
-    /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
-    public function map()
+
+    public function map(Router $router)
     {
+        $this->mapWebRoutes($router);
+
         $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-
-        //
     }
 
-    /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
-    protected function mapWebRoutes()
+
+    protected function mapWebRoutes(Router $router)
     {
-        Route::middleware('web')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+        $router->group($this->getDefaultGroup(), function () {
+            $this->requireAuthRoutes();
+            $this->requireWebRoutes();
+            $this->requireDefaultRoutes();
+        });
     }
 
-    /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
+
+    protected function getDefaultGroup()
+    {
+        return [
+            'namespace' => $this->namespace,
+            'middleware' => 'web'
+        ];
+    }
+
+
+    protected function requireAuthRoutes()
+    {
+        Route::get('/', function () {
+            if(Auth::user()) {
+                return redirect('dashboard');
+            }
+            else {
+                return view('auth.login');
+            }
+        });
+        Auth::routes();
+    }
+
+
+    protected function requireWebRoutes()
+    {
+        $files = Finder::create()->in(base_path($this->folder))->name('*.php');
+        $this->require($files);
+    }
+
+
+    protected function require($files)
+    {
+        foreach($files as $file) {
+            require $file->getRealPath();
+        }
+    }
+
+
+    protected function requireDefaultRoutes()
+    {
+        //Route::any('{query}', function() { return redirect('/');})->where('query', '.*');
+    }
+
+
     protected function mapApiRoutes()
     {
         Route::prefix('api')
-             ->middleware('api')
-             ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
     }
+
 }
