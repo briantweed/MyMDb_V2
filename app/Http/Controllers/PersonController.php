acb674ac538cc5cbd8dd1030eec31a5f891d400d
brtweed@outlook.com <brtweed@outlook.com>
2019-02-11 19:19:11 +0000

    k

---
+++ app/Http/Controllers/PersonController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Person;
+
+
+class PersonController extends Controller
+{
+
+    public function index()
+    {
+        $people = Person::byForename()->paginate();
+        return view('pages.people.index', [
+            'people' => $people
+        ]);
+    }
+
+
+    public function show(Person $person)
+    {
+        return view('pages.people.show', [
+            'person' => $person
+        ]);
+    }
+
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function edit(Person $people)
+    {
+        //
+    }
+
+
+    public function update(Request $request, Person $person)
+    {
+        //
+    }
+
+
+    public function delete(Person $person)
+    {
+        //
+    }
+
+
+    public function destroy(Person $person)
+    {
+        //
+    }
+}
