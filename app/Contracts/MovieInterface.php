db9fb2ada5473b7928aa335e4064dce276043f30
brtweed@outlook.com <brtweed@outlook.com>
2019-02-16 14:32:25 +0000

    model

---
+++ app/Contracts/MovieInterface.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Contracts;
+
+
+interface MovieInterface
+{
+
+    public const RATINGS = [
+        1 => 1,
+        2 => 2,
+        3 => 3,
+        4 => 4,
+        5 => 5,
+        6 => 6,
+        7 => 7,
+        8 => 8,
+        9 => 9,
+        10 => 10,
+    ];
+
+
+    /**
+     * Relationship - a movie can have many cast members
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function cast();
+
+
+    /**
+     * Relationship - a movie can have many main cast member, where star equals true
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function mainCast();
+
+
+    /**
+     * Relationship - a movie can have many other cast members, where star equals false
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function supportingCast();
+
+
+    /**
+     * Relationship - a movie can have many main crew members
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function crew();
+
+
+    /**
+     * Relationship - a movie belongs to a studio
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function studio();
+
+
+    /**
+     * Relationship - a movie exists on a certain format e.g. DVD, Blu-ray etc.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function format();
+
+
+    /**
+     * Relationship - a movie is given a certificate e.g. U, PG etc.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function certificate();
+
+
+    /**
+     * Relationship - a movie can belong to many types of genres
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function genres();
+
+
+    /**
+     * Relationship - a movie can have many types of tags
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags();
+
+}
